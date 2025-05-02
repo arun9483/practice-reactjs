2589d1ac1a7e6023c4270e5cff1096c7d68d1743
Arun Kumar Tripathi <arun9483@gmail.com>
2025-05-03 01:24:36 +0530

    reset tabs and re-render TabManagementContainer forcefully

--- src/day-06-tab-management/index.tsx
+++ src/day-06-tab-management/index.tsx
@@ -3,11 +3,12 @@ import { faker } from '@faker-js/faker';
 import TabManagementContainer from './TabManagementContainer';
 import { TabsManager } from './tabsManagerReducer';
 import PostRenderer, { Post } from './PostRenderer';
+import { useState } from 'react';
 
 // Function to generate a new Post dynamically
 const generateNewPost = (): Post => {
   const MIN = 1;
-  const MAX = 3
+  const MAX = 3;
   const wordCount = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN; // Random number between MIN and MAX
   return {
     id: faker.string.uuid(),
@@ -16,13 +17,27 @@ const generateNewPost = (): Post => {
   };
 };
 
-const initialState: TabsManager<Post> = {
-  tabs: Array.from({ length: 3 }, () => generateNewPost()),
-  activeTabId: null,
-};
-
 const TabManagementConsumer = () => {
-  return <TabManagementContainer<Post> initialState={initialState} renderTabContent={PostRenderer} newTabHandler={generateNewPost} />;
+  const [tabSet, setTabSet] = useState<string>(faker.string.uuid());
+
+  const tabs: Post[] = Array.from({ length: 3 }, () => generateNewPost());
+
+  const initialState: TabsManager<Post> = {
+    tabs,
+    activeTabId: tabs[tabs.length - 1].id,
+  };
+
+  return (
+    <div>
+      <button onClick={() => setTabSet(faker.string.uuid())}>Reset Tabs</button>
+      <TabManagementContainer<Post>
+        key={tabSet}
+        initialState={initialState}
+        renderTabContent={PostRenderer}
+        newTabHandler={generateNewPost}
+      />
+    </div>
+  );
 };
 
 export default TabManagementConsumer;
